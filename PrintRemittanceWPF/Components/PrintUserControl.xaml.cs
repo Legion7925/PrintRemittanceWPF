1f2dd0d256958508614c7a92ae26797ce350fd90
ZeroOne <mohamad.mirzakhani30@gmail.com>
2024-01-08 13:53:25 +0330

    feature: fill image in print user controll refactor: fix bug in show plate number in print user controll

--- PrintRemittanceWPF/Components/PrintUserControl.xaml.cs
+++ PrintRemittanceWPF/Components/PrintUserControl.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using PersianDate.Standard;
 using PrintRemittance.Core.Models;
 using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 
 namespace PrintRemittanceWPF;
 
@@ -16,10 +17,22 @@ public partial class PrintUserControl : UserControl
         txtDestination.Text = documentModel.Destination;
         txtDriverName.Text = documentModel.DriverName;
         txtFactoryName.Text = documentModel.FactoryName;
-        txtRemittanceNumber.Text =  documentModel.PlateNumber;
+        txtRemittanceNumber.Text = $"{documentModel.P4 + " " + documentModel.P3 + documentModel.P2 + documentModel.P1}";
         txtDriverName.Text= documentModel.DriverName;
         lblDate.Text = documentModel.CreatedDate.ToFa();
         lblPrintNumber.Text = documentModel.PrintNumber;
         txtProduct.Text = documentModel.Product;
+
+        FillImage();
+        
+    }
+
+    void FillImage()
+    {
+        BitmapImage myBitmapImage = new BitmapImage();
+        myBitmapImage.BeginInit();
+        myBitmapImage.UriSource = new Uri(@"D:\RepoMohammad\Test\PrintRemittanceWPF\PrintRemittanceWPF\Resources\Images\logo.png");
+        myBitmapImage.EndInit();
+        logo.Source = myBitmapImage;
     }
 }
